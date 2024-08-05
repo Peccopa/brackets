40c7142826f32f05812f60b544a6303da3b276b0
Andrey <119999253+Peccopa@users.noreply.github.com>
2024-08-05 19:56:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  return (
+    str.split('').reduce((acc, cur) => {
+      Object.fromEntries(bracketsConfig)[acc.at(-1)] === cur
+        ? acc.pop()
+        : acc.push(cur);
+      return acc;
+    }, []).length === 0
+  );
 }
